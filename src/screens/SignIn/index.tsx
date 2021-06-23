217e34a0d3367e62c5e42e099a46170660a6b37f
feliipessantos <lipe_silva_santos@hotmail.com>
2021-06-23 20:18:35 -0300

    Day 2

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,27 +1,48 @@
 import React from 'react';
-import { Image, Text, View, StatusBar } from 'react-native';
+import { 
+  View, 
+  Text, 
+  Image  
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { styles } from './styles';
-import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
+import { styles } from './styles';
 
-export function SignIn() {
-  return (
-    <View style={styles.container}>
-        <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-        />
-      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+export function SignIn(){
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate('Home');
+  }
+
+  return(
+    <View style={styles.container}>     
+      <Image 
+        source={IllustrationImg} 
+        style={styles.image} 
+        resizeMode="stretch"
+      />
 
       <View style={styles.content}>
-        <Text style={styles.title}>Organize {'\n'} suas jogatinas {'\n'} facilmente</Text>
-        <Text style={styles.subtitle}>Crie grupos para jogar seus games {'\n'} favoritos com seus amigos</Text>
+        <Text style={styles.title}>
+          Conecte-se {'\n'}
+          e organize suas {'\n'} 
+          jogatinas
+        </Text>
 
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'} 
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord"
+          onPress={handleSignIn}
+        />                               
       </View>
     </View>
   );
 }
-
